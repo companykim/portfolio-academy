fbea7944e189486f5bd2f021327cc7a13e7fb50b
companykim <giup0813@naver.com>
2023-11-10 16:20:06 +0900

    장소 검색

--- src/components/shelter/MyComponent.js
+++ src/components/shelter/MyComponent.js
@@ -1,12 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import { GoogleMap, MarkerF } from '@react-google-maps/api';
+import React, { useState, useRef, useEffect } from 'react'
+import { GoogleMap, MarkerF, MarkerClusterer, MarkerClustererF } from '@react-google-maps/api';
 import ViewDirections from './Directions';
 import ShelterMarkers_google from './ShelterMarkers_google';
-import { useRef } from 'react';
-import { render } from '@testing-library/react';
-import { MarkerClusterer } from '@react-google-maps/api';
+import "./styles.css";
 
-// 
 const containerStyle = {
   width: '100%',
   height: '600px'
@@ -23,7 +20,37 @@ const myStyles = [
 function MyComponent() {
   const [curPos, setCurPos] = useState(null);
   const [zoomLv, setZoomLv] = useState(10);
+  //장소 검색 
+  const inputRef = useRef()
+  const inputStyle= {
+    boxShadow: 'inset 0 0 10px #eee !important',
+    border: '2px solid #eee',
+    width: '456px',
+    height: '40px',
+    marginLeft: '16px',
+    borderRadius: '20px',
+    fontWeight: '300 !important',
+    outline: 'none',
+    padding: '10px 20px',
+    marginBottom: '10px',
+  }
 
+  const autoComplete = new window.google.maps.places.Autocomplete(
+    inputRef.current,
+  )
+
+  autoComplete.addListener('place_changed', () => {
+    const place = autoComplete.getPlace()
+    if (!place.geometry || !place.geometry.location) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+        alert("this location not available")
+    }
+    if (place.geometry.viewport || place.geometry.location) {
+        // do something
+        console.log(place.geometry.location)
+    }
+  })
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(geolocSuccessHandler, geoLocError); // 성공시 successHandler, 실패시 errorHandler 함수가 실행된다.
@@ -32,8 +59,6 @@ function MyComponent() {
   const geolocSuccessHandler = (response) => {
     const { latitude, longitude } = response.coords;
     setCurPos({ latitude, longitude });
-    console.log("위도: ", latitude);
-    console.log("경도: ", longitude);
   };
 
   const geoLocError = (err) => {
@@ -47,7 +72,6 @@ function MyComponent() {
 
   const toggleDirections = (e) => {
     e.preventDefault();
-    // console.log("눌리긴 하니");
     // 만약에 토글이 null이 아니면 활성화, null이면 청소하고 비활성화
     setSwichDirections(!swichDirections)
     setDestPoint(null)
@@ -73,10 +97,6 @@ function MyComponent() {
               lat: 37.4854799,
               lng: 126.8981862
             }}
-            // icon={
-            //   {url:"https://cdn-icons-gif.flaticon.com/12589/12589164.gif"}
-            // }
-            // scale={1.5}
             icon={"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"}
           >
           </MarkerF>
@@ -87,17 +107,28 @@ function MyComponent() {
             }
           </MarkerClusterer>
 
+
+
           {/* lat: curPos.latitude, lng: curPos.longitude */}
           {swichDirections && destPoint ?
             <ViewDirections origin={{ lat: 37.4854799, lng: 126.8981862 }} destination={destPoint} />
             : <></>
           }
-
         </GoogleMap>
       }
-
       <button onClick={toggleDirections}>경로 탐색</button>
+      {/* 장소 검색 */}
+      <div className="inputStyle">
+      <label >Location</label>
+        <input
+          placeholder='장소를 입력하세요'
+          ref={inputRef}
+          style={inputStyle}
+        />
+        <MarkerF
 
+        />
+      </div>
     </>
   )
 }
