6acc136bc114a40f5d75078332f6307262d10244
companykim <giup0813@naver.com>
2023-11-22 19:46:05 +0900

    호ㅗㅛㅗ

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import {Fetch} from 'toolbox/Fetch';
 
-function Home() {
+export default function Home() {
+    //const listNewsUri = `http://localhost:8080/news/anonymous/listAll`;
+    const listNewsUri = `http://localhost:9200/news*/_search?q=newstitle="검찰"andnewstitle="이재명"&pretty`;
+
     return (
         <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>기사</th>
+                        <th>날짜</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <Fetch uri={listNewsUri} renderSuccess={RenderSuccess} />
+                </tbody>
+            </table>
         </div>
     );
 }
 
-export default Home;
+function RenderSuccess(newsList) {
+    return newsList?.map(news => (
+        <tr>
+            <td>{news.newsdate}</td>
+            <td>{news.newstitle}</td>
+        </tr>
+    ))
+}
